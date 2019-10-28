650ec9090f60f90853cb5e6cdf92d52a3468eb07
Mel <jones.mel@gmail.com>
2019-10-28 21:33:49 +0100

    add save and load functions, use fs

--- homework/w2-nodejs/database.js
+++ homework/w2-nodejs/database.js
@@ -1,3 +1,16 @@
 // todo:
 // save things
 // read things
+const fs = require('fs')
+
+const save = function(filename, data) {
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2))
+  // console.log(data)
+}
+
+const load = function(filename) {
+  return JSON.parse(fs.readFileSync(filename, 'utf8'))
+  // console.log(data)
+}
+
+module.exports = { save, load }
