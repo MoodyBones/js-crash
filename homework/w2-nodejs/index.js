6bab814ad417cbb3d1c0e058cae7b0ff740e40f4
Mel <jones.mel@gmail.com>
2019-10-29 16:19:16 +0100

    edit database.save to include all records

--- homework/w2-nodejs/index.js
+++ homework/w2-nodejs/index.js
@@ -10,6 +10,7 @@ const sallySweet = new Designer('Sally Sweet', 'sallys@gmail.com')
 const farrahLee = new Designer('Farrah Lee', 'farrahl@gmail.com')
 const rosaJones = new Designer('Rosa Jones', 'rosaj@gmail.com')
 
+const designerRecord = [sallySweet, farrahLee, rosaJones]
 //////////
 
 const webDesign = new Service('Web Design')
@@ -20,11 +21,15 @@ const UIDesign = new Service('UI Design')
 const UXDesign = new Service('UX Design')
 const animation = new Service('Animation')
 
+const serviceRecord = [webDesign, typography, branding, printDesign, UIDesign, UXDesign, animation]
+
 ////////////
 
 const martin = new Customer('Martin', 'martin@email.com')
 const sam = new Customer('Sam', 'sam@email.com')
 const sarah = new Customer('Sarah', 'sarah@email.com')
+
+const customerRecord = [martin, sam, sarah]
 
 /////////// Interactions
 sallySweet.addService(webDesign)
@@ -45,9 +50,17 @@ sarah.requestService(branding, farrahLee)
 animation.printDesignersByService()
 webDesign.printDesignersByService()
 
-Database.save('service.json', animation)
-Database.save('designer.json', sallySweet)
-Database.save('customer.json', martin)
 
-const loadedFile = Database.load('designer.json')
-console.log(loadedFile.name)
+// const designerRecord = Designer.listOfAllCurrentDesigners()
+// Database.save('service.json', animation)
+// Database.save('designer.json', sallySweet)
+// Database.save('customer.json', martin)
+
+Database.save('service.json', serviceRecord)
+Database.save('designer.json', designerRecord)
+Database.save('customer.json', customerRecord)
+
+
+
+// const loadedFile = Database.load('designer.json')
+// console.log(loadedFile.name)
