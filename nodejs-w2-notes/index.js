c2447068f059379f63ec655af99594287a2786a5
Mel <jones.mel@gmail.com>
2019-10-30 19:42:14 +0100

    some extra notes on week 2

--- nodejs-w2-notes/index.js
+++ nodejs-w2-notes/index.js
@@ -1,24 +1,33 @@
+const Database = require('./database')
 const Person = require('./person')
 const Meetup = require('./meetup')
-const Database = require('./database')
+const loadedFile = Database.load('meetup.json')
 const Chalk = require('chalk')
 
-const mert = new Person('Mert', 34)
-const armagan = new Person('Armagan', 35)
+// const mert = new Person('Mert', 34)
+// const armagan = new Person('Armagan', 35)
 
-console.log(Chalk.blue.bgWhite.bold('hello world'))
+// console.log(Chalk.blue.bgWhite.bold('hello world'))
 
-mert.greet(armagan)
+// mert.greet(armagan)
 
-const wtmb = new Meetup('WTM Berlin')
+// const wtmb = new Meetup('WTM Berlin')
 
-armagan.attend(wtmb)
-mert.attend(wtmb)
+// armagan.attend(wtmb)
+// mert.attend(wtmb)
 
+// wtmb.printAttendeeNames()
+
+// Database.save('meetup.json', wtmb)
+// Database.save('person.json', mert)
+
+// const loadedFile = Database.load('meetup.json')
+// console.log(loadedFile.name)
+
+const wtmb = Meetup.create(loadedFile)
+const omur = new Person('Omur', 30)
+
+omur.attend(wtmb)
+console.log(Meetup.create({name: 'Wayfair', attendees: []}))
 wtmb.printAttendeeNames()
-
-Database.save('meetup.json', wtmb)
-Database.save('person.json', mert)
-
-const loadedFile = Database.load('meetup.json')
-console.log(loadedFile.name)
+console.log(wtmb.name)
