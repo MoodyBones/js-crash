ac3bd6882ec324c6aaae224a9c32597de1643e02
Mel <jones.mel@gmail.com>
2019-10-30 20:55:20 +0100

    rewatch vid and finsih code

--- nodejs-w2-notes/index.js
+++ nodejs-w2-notes/index.js
@@ -1,33 +1,21 @@
 const Database = require('./database')
 const Person = require('./person')
 const Meetup = require('./meetup')
-const loadedFile = Database.load('meetup.json')
-const Chalk = require('chalk')
-
-// const mert = new Person('Mert', 34)
-// const armagan = new Person('Armagan', 35)
-
-// console.log(Chalk.blue.bgWhite.bold('hello world'))
-
-// mert.greet(armagan)
-
-// const wtmb = new Meetup('WTM Berlin')
-
-// armagan.attend(wtmb)
-// mert.attend(wtmb)
-
-// wtmb.printAttendeeNames()
-
-// Database.save('meetup.json', wtmb)
-// Database.save('person.json', mert)
-
 // const loadedFile = Database.load('meetup.json')
-// console.log(loadedFile.name)
+Database.load('meetup.json', (err, loadedFile) => {
+  console.log('hello')
 
-const wtmb = Meetup.create(loadedFile)
-const omur = new Person('Omur', 30)
+  const wtmb = Meetup.create(loadedFile)
+  const omur = new Person('Omur', 30)
+  omur.attend(wtmb)
+  wtmb.printAttendeeNames()
+  console.log(wtmb.name)
+})
 
-omur.attend(wtmb)
-console.log(Meetup.create({name: 'Wayfair', attendees: []}))
-wtmb.printAttendeeNames()
-console.log(wtmb.name)
+
+
+
+
+
+
+// console.log(Meetup.create({name: 'Wayfair', attendees: []}))
