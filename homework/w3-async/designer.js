585c84e83f5bb7855eda9361e0ad6176325d1696
Mel <jones.mel@gmail.com>
2019-11-03 18:55:16 +0100

    adding static create()

--- homework/w3-async/designer.js
+++ homework/w3-async/designer.js
@@ -4,17 +4,13 @@ module.exports = class Designer {
   constructor(name, email) {
     this.name = name
     this.email = email
-    this.skills = []
-    this.requests = []
   }
 
   addSkill(skill) {
-    this.skills.push(skill)
-    skill.designers.push([this.name, this.email])
-    // console.log(
-    //   `${Chalk.blue(this.name)} has added ${Chalk.green(
-    //     skill.name
-    //   )} as an available skill`
-    // )
+    skill.designers.push(this)
+  }
+
+  static create({ name, email }) {
+    return new Designer(name, email)
   }
 }
