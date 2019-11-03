9c8b7c583100f3c499519477e8830cad515fddb2
Mel <jones.mel@gmail.com>
2019-11-03 21:09:17 +0100

    refactor inquireSkill() and requestDesigner()

--- homework/w3-async/customer.js
+++ homework/w3-async/customer.js
@@ -1,32 +1,31 @@
 const Chalk = require('chalk')
 
 module.exports = class Customer {
-  constructor(name, email) {
+  constructor(name, email, inquiries = [], requests = []) {
     this.name = name
     this.email = email
-    this.inquiry = []
-    this.request = []
-  }
-
-  requestDesigner(skill, designer) {
-    this.request.push([skill, designer])
-    designer.requests.push([[this.name, this.email], skill])
-    // console.log(
-    //   `Hey ${
-    //     designer.name
-    //   }, I'm seeking a designer with experience in ${Chalk.green(
-    //     skill.name
-    //   )}, are you available to help? Kind regards, ${this.name}`
-    // )
+    this.inquiries = inquiries
+    this.requests = requests
   }
 
   inquireSkill(skill) {
-    if (skill.name) {
-      this.inquiry.push([skill, skill.designers])
-      console.log(
-        `The following designers have experience in ${Chalk.green(skill.name)}:`
-      )
-      console.log(skill.designers)
-    }
+    // return a list of designers with that skill
+    // use sort to on list of returned designers
+    // const sortDesigners = skill.designers.sort((a, b) => (a > b ? 1 : -1))
+    this.inquiries.push(skill.name, skill.designers)
+
+    // console.log(skill.name, sortDesigners) // testing
+    // console.log('inquiry', this.name, this.inquiries)
+  }
+
+  requestDesigner(skill, designer) {
+    this.requests.push(skill.name, designer.name, designer.email)
+    designer.requests.push(skill.name, this.name, this.email)
+    // console.log('customer', this.requests)
+    // console.log('designer', designer.requests)
+  }
+
+  static create({ name, email, inquiries, requests }) {
+    return new Customer(name, email, inquiries, requests)
   }
 }
