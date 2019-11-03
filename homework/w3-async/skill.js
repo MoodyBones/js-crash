cda15d2218811afd009a89da88a41296d0163123
Mel <jones.mel@gmail.com>
2019-11-03 21:13:17 +0100

    and static functions

--- homework/w3-async/skill.js
+++ homework/w3-async/skill.js
@@ -1,21 +1,21 @@
 const Chalk = require('chalk')
 
 module.exports = class Skill {
-  constructor(name) {
+  constructor(name, designers = []) {
     this.name = name
-    this.designers = []
+    this.designers = designers
   }
 
+  // not sure about this..
   printDesignersBySkill() {
-    if (this.designers.length === 0) {
-      console.log(
-        `Sorry, we don't have any designers that are experienced in ${this.name}.`
-      )
-    } else {
-      console.log(`The following designers have experience in ${this.name}:`)
-      this.designers.forEach(printName)
-    }
+    const printName = person => console.log(Chalk.blue(person))
+    console.log(`The following designers have experience in ${this.name}:`)
+    this.designers.forEach(printName)
+  }
+
+  static create({ name, designers }) {
+    const skill = new Skill(name, designers)
+    skill.designers = designers.map(Designers.create)
+    return skill
   }
 }
-
-const printName = person => console.log(Chalk.blue(person))
