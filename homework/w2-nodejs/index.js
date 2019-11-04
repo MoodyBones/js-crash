a123a3f197f03b987e7ab27c01f4eaa476ded27a
Mel <jones.mel@gmail.com>
2019-11-04 14:54:04 +0100

    adding testing stuff

--- homework/w2-nodejs/index.js
+++ homework/w2-nodejs/index.js
@@ -72,3 +72,113 @@ const loadedCustomers = Database.load('customer.json')
 console.log(loadedServices)
 console.log(loadedDesigners)
 console.log(loadedCustomers)
+
+
+//////// Tesing stuffsss
+////// Classes
+/////// Designer
+
+// const sallySweet = new Designer('Sally Sweet', 'sallys@gmail.com')
+// const farrahLee = new Designer('Farrah Lee', 'farrahl@gmail.com')
+// const rosaJones = new Designer('Rosa Jones', 'rosaj@gmail.com')
+
+// let designerRecord = [sallySweet, farrahLee, rosaJones]
+
+// //////// Skill
+
+// const webDesign = new Skill('Web Design')
+// const typography = new Skill('Typography')
+// const branding = new Skill('Branding')
+// const printDesign = new Skill('Print Design')
+// const UIDesign = new Skill('UI Design')
+// const UXDesign = new Skill('UX Design')
+// const animation = new Skill('Animation')
+
+// let skillRecord = [
+//   webDesign,
+//   typography,
+//   branding,
+//   printDesign,
+//   UIDesign,
+//   UXDesign,
+//   animation,
+// ]
+
+// ////////// Customer
+
+// const martin = new Customer('Martin', 'martin@email.com')
+// const sam = new Customer('Sam', 'sam@email.com')
+// const sarah = new Customer('Sarah', 'sarah@email.com')
+
+// let customerRecord = [martin, sam, sarah]
+
+// ///////// Interactions
+
+// sallySweet.addSkill(webDesign)
+// sallySweet.addSkill(UXDesign)
+// sallySweet.addSkill(animation)
+// rosaJones.addSkill(animation)
+// rosaJones.addSkill(typography)
+// rosaJones.addSkill(UIDesign)
+// farrahLee.addSkill(typography)
+// farrahLee.addSkill(printDesign)
+// farrahLee.addSkill(branding)
+
+// sarah.inquireSkill(animation)
+// sam.inquireSkill(typography)
+// sam.inquireSkill(animation)
+
+// martin.requestDesigner(webDesign, sallySweet)
+// martin.requestDesigner(animation, sallySweet)
+// sam.requestDesigner(typography, rosaJones)
+// sam.requestDesigner(UIDesign, rosaJones)
+// sarah.requestDesigner(branding, farrahLee)
+
+// animation.printDesignersBySkill()
+// webDesign.printDesignersBySkill()
+
+
+//////// testing 
+
+// const conor = new Designer('Conor Rad', 'conor@email.com')
+// const arne = new Designer('Arne Adventure', 'arne@email.com')
+// // console.log(conor, arne)
+
+// const photography = new Skill('Photography')
+// conor.addSkill(photography)
+// arne.addSkill(photography)
+// photography.printDesignersBySkill()
+
+// skillRecord = [
+//   webDesign,
+//   typography,
+//   branding,
+//   printDesign,
+//   UIDesign,
+//   UXDesign,
+//   animation,
+//   photography
+// ]
+
+// designerRecord = [sallySweet, farrahLee, rosaJones, conor, arne]
+
+// Database.save('skill.json', photography)
+// const loadedFile = Database.load('skill.json')
+// console.log(loadedFile.designers[0].name)
+
+
+// /////////////////// SAVE
+
+// Database.save('skill.json', skillRecord)
+// Database.save('designer.json', designerRecord)
+// Database.save('customer.json', customerRecord)
+
+/////////////////////// LOAD
+
+// const loadedSkills = Database.load('skill.json')
+// const loadedDesigners = Database.load('designer.json')
+// const loadedCustomers = Database.load('customer.json')
+// const skill = Skill.create(loadedSkills)
+// console.log(skill)
+// console.log(loadedDesigners)
+// console.log(loadedCustomers)
