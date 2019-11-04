f37fca721857a7cd9b9b99415aa6e7e55ada979e
Mel <jones.mel@gmail.com>
2019-11-04 11:14:58 +0100

    refactor database

--- homework/w3-async/database.js
+++ homework/w3-async/database.js
@@ -1,18 +1,28 @@
 const fs = require('fs')
 
-const save = function(filename, data) {
-  fs.writeFile(filename, JSON.stringify(data, null, 2))
-}
+// const save = function(filename, data) {
+//   fs.writeFile(filename, JSON.stringify(data, null, 2))
+// }
 
-const load = function(filename, handler) {
-  fs.readFile(filename, 'utf8', (err, file) => {
-    if (err) {
-      console.log('there is a read error', err)
-      handler(err)
-      return
-    }
-    handler(null, JSON.parse(file))
-  })
-}
+// const load = function(filename, handler) {
+//   fs.readFile(filename, 'utf8', (err, file) => {
+//     if (err) {
+//       console.log('there is a read error', err)
+//       handler(err)
+//       return
+//     }
+//     handler(null, JSON.parse(file))
+//   })
+// }
 
-module.exports = { save, load }
+// module.exports = { save, load }
+
+
+module.exports = {
+  save(filename, data) {
+    fs.writeFileSync(filename, JSON.stringify(data, null, 2))
+  },
+  load(filename) {
+    return JSON.parse(fs.readFileSync(filename))
+  },
+}
