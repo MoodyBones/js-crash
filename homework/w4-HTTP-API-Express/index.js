5ef06ed05e8be654b562e1001c755e9640b06b53
Mel <jones.mel@gmail.com>
2019-11-07 19:19:07 +0100

    stuck here: app.post() bug

--- homework/w4-HTTP-API-Express/index.js
+++ homework/w4-HTTP-API-Express/index.js
@@ -9,114 +9,6 @@
 
 // console.log('Hello World!! this is Mel Jones')
 
-// async function main() {
-//   ////////// add Designers
-//   const sallySweet = new Designer('Sally Sweet', 'sallys@gmail.com')
-//   const farrahLee = new Designer('Farrah Lee', 'farrahl@gmail.com')
-//   const rosaJones = new Designer('Rosa Jones', 'rosaj@gmail.com')
-
-//   ////////// add Skills
-//   const webDesign = new Skill('Web Design')
-//   const typography = new Skill('Typography')
-//   const branding = new Skill('Branding')
-//   const printDesign = new Skill('Print Design')
-//   const uiDesign = new Skill('UI Design')
-//   const uxDesign = new Skill('UX Design')
-//   const animation = new Skill('Animation')
-
-//   ////// and some interactions
-//   sallySweet.addSkill(webDesign)
-//   sallySweet.addSkill(uxDesign)
-//   sallySweet.addSkill(animation)
-//   rosaJones.addSkill(animation)
-//   rosaJones.addSkill(typography)
-//   rosaJones.addSkill(uiDesign)
-//   farrahLee.addSkill(typography)
-//   farrahLee.addSkill(printDesign)
-//   farrahLee.addSkill(branding)
-
-//   //////// push new designers to DB
-//   await DesignerService.add(sallySweet)
-//   await DesignerService.add(farrahLee)
-//   await DesignerService.add(rosaJones)
-
-//   //////// test DesignerService
-//   const designers = await DesignerService.findAll()
-
-//   console.log(designers)
-
-//   animation.printDesignersBySkill()
-//   webDesign.printDesignersBySkill()
-
-//   //////// push new skills to DB
-//   await SkillService.add(webDesign)
-//   await SkillService.add(typography)
-//   await SkillService.add(branding)
-//   await SkillService.add(printDesign)
-//   await SkillService.add(uiDesign)
-//   await SkillService.add(uxDesign)
-//   await SkillService.add(animation)
-
-//   ///////// test SkillService
-//   const skills = await SkillService.findAll()
-
-//   console.log(skills)
-
-//   //////// test adding additional new designers and skills
-//   const conor = new Designer('Conor Rad', 'conor@email.com')
-//   const arne = new Designer('Arne Adventure', 'arne@email.com')
-
-//   await DesignerService.add(conor)
-//   await DesignerService.add(arne)
-
-//   const designersMore = await DesignerService.findAll()
-
-//   console.log(designersMore)
-
-//   const photography = new Skill('Photography')
-//   await SkillService.add(photography)
-
-//   conor.addSkill(photography)
-//   arne.addSkill(photography)
-
-//   photography.printDesignersBySkill()
-
-//   ///////////// add Customers
-//   const martin = new Customer('Martin', 'martin@email.com')
-//   const sam = new Customer('Sam', 'sam@email.com')
-//   const sarah = new Customer('Sarah', 'sarah@email.com')
-
-//   await CustomerService.add(martin)
-//   await CustomerService.add(sam)
-//   await CustomerService.add(sarah)
-
-//   sarah.inquireSkill(animation)
-//   sam.inquireSkill(typography)
-//   sam.inquireSkill(animation)
-
-//   martin.requestDesigner(webDesign, sallySweet)
-//   martin.requestDesigner(animation, sallySweet)
-//   sam.requestDesigner(typography, rosaJones)
-//   sam.requestDesigner(uiDesign, rosaJones)
-//   sarah.requestDesigner(branding, farrahLee)
-
-//   //////// test CustomerService
-//   const customers = await CustomerService.findAll()
-
-//   // console.log(customers)
-
-//   console.log('it should say Martin..', Chalk.red(customers[0].name))
-
-//   await CustomerService.del(1)
-
-//   const testDeletedCust = await CustomerService.findAll()
-
-//   console.log('it should say Sam..', Chalk.red(testDeletedCust[0].name))
-
-// }
-
-// main()
-
 ///////// WEEK 4
 const express = require('express')
 const DesignerService = require('./services/designer-service')
@@ -144,13 +36,20 @@ app.get('/designer/:id', async (req, res) => {
 })
 
 app.post('/person', async (req, res) => {
-  const designer = await DesignerService.add(req.body)
-  res.send(designer)
+  console.log(req.body)
+  // !!! Error in chrome devtools console when I enter: 
+  // axios.post('/designer', { name: 'Mel', email: 'mel@gmail.com' }).then(console.log)
+
+  // Console returns:
+  // [[PromiseStatus]]: "rejected"
+  // [[PromiseValue]]: Error: Request failed with status code 404 at e.exports
+
+  // const designer = await DesignerService.add(req.body)
+  // res.send(designer)
 })
 
 // app.delete('/designer/:id', async (req, res) => {
-//   const id = req.params.id
-//   await Perso
+
 // })
 
 app.listen(3000, () => {
