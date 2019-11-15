38c4f91559e4dc45f5162ddf273cc67d60e70c2d
Mel <jones.mel@gmail.com>
2019-11-15 18:51:38 +0100

    mongooseing designer model

--- homework/w5-mongoDb/models/designer.js
+++ homework/w5-mongoDb/models/designer.js
@@ -1,26 +1,73 @@
-module.exports = class Designer {
-  constructor(name, email, website, city, country, skills = [], requests = [], id) {
-    this.name = name
-    this.email = email
-    this.website = website
-    this.city = city
-    this.country = country
-    this.skills = skills
-    this.requests = requests
-    this.id = id
-  }
+const mongoose = require("mongoose")
 
-  addSkill(skill) {
-    // console.log(this.name, skill.name) //testing
-    this.skills.push(skill.name)
-    // this.skills.push(skill)
-    skill.designers.push(this.name)
-    // skill.designers.push(this)
-  }
+const DesignerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  website: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  skills: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Skills",
+      autopopulate: {
+        maxDepth: 1
+      }
+    }
+  ]
+})
 
-  
+// DesignerSchema.methods.findPeersOver18 = function(cb) {
+//   return DesignerModel.find({
+//     age: {
+//       $gte: 18
+//     }
+//   })
+// }
 
-  static create({ name, email, website, city, country, skills, requests, id }) {
-    return new Designer(name, email, website, city, country, skills, requests, id)
-  }
-}
+DesignerSchema.plugin(require("mongoose-autopopulate"))
+
+const DesignerModel = mongoose.model("Designer", DesignerSchema)
+
+module.exports = DesignerModel
+
+// module.exports = class Designer {
+//   constructor(name, email, website, city, country, skills = [], requests = [], id) {
+//     this.name = name
+//     this.email = email
+//     this.website = website
+//     this.city = city
+//     this.country = country
+//     this.skills = skills
+//     this.requests = requests
+//     this.id = id
+//   }
+
+//   // addSkill(skill) {
+//   //   // console.log(this.name, skill.name) //testing
+//   //   this.skills.push(skill.name)
+//   //   // this.skills.push(skill)
+//   //   skill.designers.push(this.name)
+//   //   // skill.designers.push(this)
+//   // }
+
+//   static create({ name, email, website, city, country, skills, requests, id }) {
+//     return new Designer(name, email, website, city, country, skills, requests, id)
+//   }
+// }
