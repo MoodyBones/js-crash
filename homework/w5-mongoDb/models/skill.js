6e21976fe9689681c85d25e976bfc08e18380bea
Mel <jones.mel@gmail.com>
2019-11-15 18:59:40 +0100

    refactor skill model

--- homework/w5-mongoDb/models/skill.js
+++ homework/w5-mongoDb/models/skill.js
@@ -1,20 +1,42 @@
-const Chalk = require('chalk')
+const mongoose = require("mongoose")
 
-module.exports = class Skill {
-  constructor(name, designers = [], id) {
-    this.name = name
-    this.designers = designers
-    this.id = id
-  }
+const SkillSchema = new mongoose.Schema({
+  name: String,
+  designers: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Designers",
+      autopopulate: {
+        maxDepth: 1
+      }
+    }
+  ]
+})
 
-  printDesignersBySkill() {
-    console.log(`The following designers have experience in ${Chalk.green(this.name)}:`)
-    return this.designers.forEach(designer => {
-      console.log(Chalk.blue(designer.name))
-    })
-  }
+SkillSchema.plugin(require("mongoose-autopopulate"))
 
-  static create({ name, designers, id }) {
-    return new Skill(name, designers, id)
-  }
-}
+const SkillModel = mongoose.model("Skill", SkillSchema)
+
+module.exports = SkillModel
+
+
+// const Chalk = require('chalk')
+
+// module.exports = class Skill {
+//   constructor(name, designers = [], id) {
+//     this.name = name
+//     this.designers = designers
+//     this.id = id
+//   }
+
+//   printDesignersBySkill() {
+//     console.log(`The following designers have experience in ${Chalk.green(this.name)}:`)
+//     return this.designers.forEach(designer => {
+//       console.log(Chalk.blue(designer.name))
+//     })
+//   }
+
+//   static create({ name, designers, id }) {
+//     return new Skill(name, designers, id)
+//   }
+// }
