02a0f5e81db9b9049981077a6fd6bf5caa9dd441
Mel <jones.mel@gmail.com>
2019-11-15 19:14:19 +0100

    set up skill router

---
+++ homework/w5-mongoDb/routes/skill.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+
+const SkillService = require("../services/skill-service")
+
+router.get("/all", async (req, res) => {
+  const skills = await SkillService.findAll()
+  res.render("list", { items: skills })
+})
+
+router.get("/:id", async (req, res) => {
+  const skill = await SkillService.find(req.params.id)
+  res.render("data", { data: skill })
+})
+
+router.post("/", async (req, res) => {
+  const skill = await SkillService.add(req.body)
+  res.send(skill)
+})
+
+router.delete("/:id", async (req, res) => {
+  const skill = await SkillService.del(req.params.id)
+  res.send(skill
+})
+
+module.exports = router
