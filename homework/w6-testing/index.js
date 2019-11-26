55fca7c54302315f9b2b6c04b98f3e796a68e298
Mel <jones.mel@gmail.com>
2019-11-26 14:00:44 +0100

    comment out searchRoutrer

--- homework/w6-testing/index.js
+++ homework/w6-testing/index.js
@@ -6,7 +6,7 @@ const designerRouter = require('./routes/designer')
 const skillRouter = require('./routes/skill')
 const locationRouter = require('./routes/location')
 const userRouter = require('./routes/user')
-const searchRouter = require('./routes/search')
+// const searchRouter = require('./routes/search')
 
 require('./mongo-connection')
 
@@ -21,7 +21,7 @@ app.use('/designer', designerRouter)
 app.use('/skill', skillRouter)
 app.use('/location', locationRouter)
 app.use('/user', userRouter)
-app.use('/search', searchRouter)
+// app.use('/search', searchRouter)
 
 app.get('/', (req, res) => {
   res.render('index')
