8e11349db13134ded9f178a366d4072ec3f949ea
Mel <jones.mel@gmail.com>
2019-11-26 17:43:23 +0100

    refactor with try/catch, tested with axios OK

--- homework/w6-testing/routes/designer.js
+++ homework/w6-testing/routes/designer.js
@@ -5,41 +5,91 @@ const router = express.Router()
 const DesignerService = require('../services/designer-service')
 const SkillService = require('../services/skill-service')
 const LocationService = require('../services/location-service')
-const SearchService = require('../services/location-service')
 
 router.get('/all', async (req, res) => {
-  const people = await DesignerService.findAll()
-  res.render('list', { items: people })
+  try {
+    const users = await DesignerService.findAll()
+    res.render('list', { items: users })
+  } catch (err) {
+    console.error(err.message)
+    res.status(404).send(`Server error: ${err.message}`)
+  }
 })
 
 router.get('/:id', async (req, res) => {
-  const user = await DesignerService.find(req.params.id)
-  res.render('data', { data: user })
+  const { id } = req.params
+  try {
+    const user = await DesignerService.find(id)
+    if (!user) {
+      res.status(404).send(`Error: Could not find designer for id >${id}<`)
+    } else {
+      res.render('data', { data: user })
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send(`Server error: ${err.message}`)
+  }
 })
 
 router.post('/', async (req, res) => {
-  const user = await DesignerService.add(req.body)
-  res.send(user)
+  try {
+    const user = await DesignerService.add(req.body)
+    res.send(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send(`Server error: ${err.message}`)
+  }
 })
 
 router.delete('/:id', async (req, res) => {
-  const user = await DesignerService.del(req.params.id)
-  res.send(user)
+  const { id } = req.params
+  try {
+    const user = await DesignerService.del(id)
+    if (!user) {
+      res.status(404).send(`Error: Could not find designer for id >${id}<`)
+    } else {
+      res.send('User deleted OK')
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send(`Server error: ${err.message}`)
+  }
 })
 
 router.post('/:id/skills', async (req, res) => {
-  const user = await DesignerService.find(req.params.id)
-  const skill = await SkillService.find(req.body.skill)
-  await DesignerService.addSkills(user, skill)
-  res.send(user)
+  try {
+    const user = await DesignerService.find(req.params.id)
+    const skill = await SkillService.find(req.body.skill)
+    if (!user) {
+      res.status(404).send(`Error: Could not find DESIGNER for id >${id}<`)
+    } else if (!skill) {
+      res.status(404).send(`Error: Could not find SKILL for id >${id}<`)
+    } else {
+      await DesignerService.addSkills(user, skill)
+      res.send(user)
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send(`Server error: ${err.message}`)
+  }
 })
 
 router.post('/:id/location', async (req, res) => {
-  const user = await DesignerService.find(req.params.id)
-  const location = await LocationService.find(req.body.location)
-  await DesignerService.setLocations(user, location)
-  res.send(user)
+  try {
+    const user = await DesignerService.find(req.params.id)
+    const location = await LocationService.find(req.body.location)
+    if (!user) {
+      res.status(404).send(`Error: Could not find DESIGNER for id >${id}<`)
+    } else if (!location) {
+      res.status(404).send(`Error: Could not find LOCATION for id >${id}<`)
+    } else {
+      await DesignerService.setLocations(user, location)
+      res.send(user)
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send(`Server error: ${err.message}`)
+  }
 })
-
 
 module.exports = router
