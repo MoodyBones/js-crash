90882428383155704ebf690c87b77c88a53bc515
Mel <jones.mel@gmail.com>
2019-11-26 14:26:37 +0100

    useCreateIndex: true

--- homework/w6-testing/mongo-connection.js
+++ homework/w6-testing/mongo-connection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 async function main() {
   await mongoose.connect('mongodb://localhost/udd', {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
   })
   console.log('connected')
 }
