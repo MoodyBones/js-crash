eb05e35787c561c07c8461839051bfdb1fc2881c
Mel <jones.mel@gmail.com>
2019-12-06 19:04:08 +0100

    adding cors

--- homework/w7-vue/backend/app.js
+++ homework/w7-vue/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const path = require('path')
+// const path = require('path') // can i remove this now? i think yes!?
+const cors = require('cors')
 
 const designerRouter = require('./routes/designer')
 const skillRouter = require('./routes/skill')
@@ -10,11 +11,12 @@ const userRouter = require('./routes/user')
 require('./mongo-connection')
 
 const app = express()
+app.use(cors())
 
 app.set('view engine', 'pug')
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public'))) // remove this
 
 app.use('/designer', designerRouter)
 app.use('/skill', skillRouter)
