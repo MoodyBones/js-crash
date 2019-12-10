28752b5c61099123af8702787e6e6b58a0f2dd93
Mel <jones.mel@gmail.com>
2019-12-10 15:16:53 +0100

    add skill

--- homework/w9-final/frontend/src/store/index.js
+++ homework/w9-final/frontend/src/store/index.js
@@ -8,24 +8,24 @@ export default new Vuex.Store({
   state: {
     designers: [],
     designer: {},
-    counter: 0
+    skills: [],
+    skill: []
   },
   mutations: {
-    SET_COUNTER(state, newCount) {
-      state.counter = newCount
-    },
     SET_DESIGNERS(state, data) {
       state.designers = data
     },
     SET_DESIGNER(state, data) {
       state.designer = data
+    },
+    SET_SKILLS(state, data) {
+      state.skills = data
+    },
+    SET_SKILL(state, data) {
+      state.skill = data
     }
   },
   actions: {
-    incrementCounter({ commit, state }) {
-      const newCount = state.counter + 1
-      commit('SET_COUNTER', newCount)
-    },
     async fetchDesigners({ commit }) {
       const result = await axios.get(`http://localhost:3000/designer/all/json`)
       commit('SET_DESIGNERS', result.data)
@@ -35,6 +35,16 @@ export default new Vuex.Store({
         `http://localhost:3000/designer/${id}/json`
       )
       commit('SET_DESIGNER', result.data)
+    },
+    async fetchSkills({ commit }) {
+      const result = await axios.get(`http://localhost:3000/skill/all/json`)
+      commit('SET_SKILLS', result.data)
+    },
+    async fetchSkill({ commit }, id) {
+      const result = await axios.get(
+        `http://localhost:3000/skill/${id}/json`
+      )
+      commit('SET_SKILL', result.data)
     }
   },
   modules: {}
